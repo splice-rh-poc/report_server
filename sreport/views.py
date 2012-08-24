c21cd6b865606ca5822746625384679eb25b147e
Wes Hayutin <whayutin@redhat.com>
2012-08-24 11:41:41 -0400

    first cut of a real report

--- sreport/views.py
+++ sreport/views.py
@@ -10,9 +10,10 @@ from django.contrib.auth import (login as auth_login,
     logout as auth_logout, authenticate)
 from django.template import RequestContext
 from sreport.models import ConsumerIdentityForm, ProductUsageForm
-from sreport.models import ProductUsage, ConsumerIdentity
+from sreport.models import ProductUsage, ConsumerIdentity, ReportingItem, MarketingProduct
 from django.template.response import TemplateResponse
 from django.forms.models import model_to_dict
+from kitchen.pycompat25.collections._defaultdict import defaultdict
 
 
 
@@ -60,7 +61,7 @@ def login(request):
     if user is not None:
         if user.is_active:
             auth_login(request, user)
-            return template_response(request, 'create_report/create.html')
+            return template_response(request, 'create_report/create_report.html')
         else:
             pass
             # Return a 'disabled account' error message
@@ -75,7 +76,9 @@ def logout(request):
 def index(request):
     return template_response(request, 'base.html')
 
-def create(request):
+
+    
+def create_filter(request):
     #ReportFormSet = formset_factory( ProductUsageForm)
     if request.method == 'POST':
         form = ProductUsageForm(request.POST)
@@ -86,19 +89,40 @@ def create(request):
             consumer = request.POST['consumer.value']
             
             results = filtered_results(consumer)
-            return template_response(request, 'create_report/list.html')
+            return template_response(request, 'create_report/machine.html')
     else:
         form = ProductUsageForm()
         return render_to_response('create_report/create.html', {'form': form})
 
-def results(request):
+def create_report(request):
+    #ReportFormSet = formset_factory( ProductUsageForm)
+    if request.method == 'POST':
+        form = ProductUsageForm(request.POST)
+        if form.is_valid():
+            #start_date = request.POST['startDate']
+            #end_date = request.POST['endDate']
+            #splice_server = request.POST['splice_server']
+            consumer = request.POST['consumer.value']
+            
+            results = hours_per_consumer(consumer)
+            return template_response(request, 'create_report/report.html')
+    else:
+        form = ProductUsageForm()
+        return render_to_response('create_report/create_report.html', {'form': form})
+
+
+
+def machine_results(request):
     consumer = request.GET['consumer']
     results = filtered_results(consumer)
-    
-    
     response = TemplateResponse(request, 'create_report/list.html', {'list': results})
     return response
-    
+
+def report(request):
+    consumer = request.GET['consumer']
+    results = hours_per_consumer(consumer)
+    response = TemplateResponse(request, 'create_report/report.html', {'list': results})
+    return response
 
 
 def filtered_results(consumer):
@@ -115,6 +139,40 @@ def filtered_results(consumer):
                 product_list.append(p)
             filtered.append({'mac': mac, 'hostname': hostname, 'uuid': uuid, "products": product_list, 'consumer_id': consumer_id})
     return filtered
+
+def hours_per_consumer(consumer):
+    results = []
+    all =  ProductUsage.objects.all()
+    product_usage = defaultdict(int)
+    for i in all:
+        
+        if str(i.consumer.id) == consumer:
+            #product_usage = {}
+            
+            for p in i.product_info:
+                date = p._data['date']
+                product_name = p.product.name
+                
+                product_name_str = str(product_name.encode('ascii'))
+                
+                product_usage[product_name_str] += 1
+        
+                    
+                
+    for key, value in product_usage.items():
+        details = {}
+        details['name'] = key
+        details['checkins'] = value
+        details['sla'] = 'Premium'
+        details['support'] = 'L1-L3'
+        details['facts'] = 'RAM < 8GB'
+        details['contract_use'] = '100'
+        results.append(details)
+    return results
+            
+        
+        
+    return results
 
 
     
