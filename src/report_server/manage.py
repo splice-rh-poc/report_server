cc2143b06a613c04fdd8ab15759c2787ef602d5a
James Slagle <jslagle@redhat.com>
2012-11-21 16:25:11 -0500

    updates

--- src/report_server/manage.py
+++ src/report_server/manage.py
@@ -3,10 +3,6 @@ import os
 import sys
 
 
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(curr_dir, '..'))
-
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dev.settings")
 
