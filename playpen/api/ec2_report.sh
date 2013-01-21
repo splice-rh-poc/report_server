772a47ac19cd942743782dbc3c1cd0eb187d1b95
Wes Hayutin <whayutin@redhat.com>
2013-01-21 14:46:03 -0500

    blanking out config

---
+++ playpen/api/ec2_report.sh
@@ -0,0 +1,1 @@
+curl -u shadowman@redhat.com:shadowman@redhat.com -k --request POST --data @report_json01  https://ec2-23-22-55-87.compute-1.amazonaws.com/report-server/api/v1/report/ --trace-ascii curl_log
