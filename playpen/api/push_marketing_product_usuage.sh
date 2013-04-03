cfdea147d317bdd6514a1c5ddaf0c4735ad71489
John Matthews <jwmatthews@gmail.com>
2013-04-03 18:06:09 -0400

    Update playpen script and sample data for MarketingProductUsage

--- playpen/api/push_marketing_product_usuage.sh
+++ playpen/api/push_marketing_product_usuage.sh
@@ -1,1 +1,12 @@
-curl -k --request POST --data @marketing_product_usage.mock.example http://localhost:8000/api/v1/marketingproductusage/ --trace-ascii curl_log
+#!/bin/sh
+
+export SERVER_ADDR=`hostname`
+export PORT=8000
+export CA_CERT=/etc/pki/splice/generated/Splice_HTTPS_CA.cert
+export CLIENT_CERT=/etc/pki/consumer/Splice_identity.cert
+export CLIENT_KEY=/etc/pki/consumer/Splice_identity.key
+export SAMPLE_JSON=./marketing_product_usage.example
+
+#curl -s -S --cacert ${CA_CERT} --cert ${CLIENT_CERT} --key ${CLIENT_KEY} --dump-header - -H "Content-Type: application/json" -X POST --data @${SAMPLE_JSON} https://${SERVER_ADDR}:${PORT}/api/v1/marketingproductusage/  --trace-ascii curl_log
+
+curl --dump-header - -H "Content-Type: application/json" -X POST --data @${SAMPLE_JSON} http://${SERVER_ADDR}:${PORT}/api/v1/marketingproductusage/  --trace-ascii curl_log
