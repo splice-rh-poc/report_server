4b0ef67f2cdda7e9b70a50fe8e3066db9fb63ee3
Wes Hayutin <whayutin@redhat.com>
2013-04-03 12:02:08 -0400

    add sla

--- playpen/api/push_marketing_product_usuage.sh
+++ playpen/api/push_marketing_product_usuage.sh
@@ -1,1 +1,1 @@
-curl -k --request POST --data @marketing_product_usage.example http://localhost:8000/api/v1/marketingproductusage/ --trace-ascii curl_log
+curl -k --request POST --data @marketing_product_usage.mock.example http://localhost:8000/api/v1/marketingproductusage/ --trace-ascii curl_log
