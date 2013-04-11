065370baa510343b6cb7839ab55a0baf56e8ef51
John Matthews <jwmatthews@gmail.com>
2013-04-11 09:14:56 -0400

    Updated curl scripts to not use cacert when uploading data to splice.common.apis

--- playpen/api/rpm_deployment/push_rules.sh
+++ playpen/api/rpm_deployment/push_rules.sh
@@ -7,5 +7,6 @@ export CLIENT_CERT=/etc/pki/consumer/Splice_identity.cert
 export CLIENT_KEY=/etc/pki/consumer/Splice_identity.key
 export SAMPLE_JSON=../rules.json
 
-curl -s -S --cacert ${CA_CERT} --cert ${CLIENT_CERT} --key ${CLIENT_KEY} --dump-header - -H "Content-Type: application/json" -X POST --data @${SAMPLE_JSON} https://${SERVER_ADDR}:${PORT}/report-server/api/v1/rules/  --trace-ascii curl_log
+#curl -s -S --cacert ${CA_CERT} --cert ${CLIENT_CERT} --key ${CLIENT_KEY} --dump-header - -H "Content-Type: application/json" -X POST --data @${SAMPLE_JSON} https://${SERVER_ADDR}:${PORT}/report-server/api/v1/rules/  --trace-ascii curl_log
+curl -s -S -k --cert ${CLIENT_CERT} --key ${CLIENT_KEY} --dump-header - -H "Content-Type: application/json" -X POST --data @${SAMPLE_JSON} https://${SERVER_ADDR}:${PORT}/report-server/api/v1/rules/  --trace-ascii curl_log
 
