a577a6bdde65f1496509ddfb0b3fc6185af71bff
Wes Hayutin <whayutin@redhat.com>
2013-04-24 13:35:56 -0400

    ruby mongo driver should have everything we need

--- playpen/ruby/test.rb
+++ playpen/ruby/test.rb
@@ -15,6 +15,22 @@ print "Print one object" << space
 one = @coll.find_one
 print one.to_s << space
 
-print "Total number of objects in DB: " << @coll.count.to_s
+print "Total number of objects in DB: " << @coll.count.to_s << space
+
+print "Find objects w/ invalid status: "  
+status = @coll.find({"status" => "invalid"}, :fields => ["status"]).to_a
+print status.to_s << space
+
+print "Find objects by date range:"
+date_range = @coll.find({"date" => {"$gt" => Time.utc(2013, 03, 05), "$lt" => Time.utc(2013, 03, 07)}}, :fields => ["date"]).to_a
+print date_range.to_s << space
+
+print "Find objects by date range and status:"
+date_range = @coll.find({"date" => {"$gt" => Time.utc(2013, 03, 05), "$lt" => Time.utc(2013, 03, 07)}, "status" => "partial"}, :fields => ["date", "status"]).to_a
+print date_range.to_s << space
+
+print "Find objects by id: "
+by_id = @coll.find({"_id" => BSON::ObjectId('5165c848421aa99a4e00000a')}, :fields => ["_id"]).to_a
+print by_id.to_s << space
 
 
